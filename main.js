2f31bff4186d37ace45661ba6230d6d8e373c0c9
DPS0340 <jh001007@gmail.com>
2019-08-01 00:11:52 +0900

    refactor

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const div = document.getElementById("showcase");
 const colortable = ["AliceBlue","AntiqueWhite","Aqua","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
 const days = ["월", "화", "수", "목", "금", "토", "일"];
 
@@ -6,11 +5,12 @@ let colori = 0
 
 
 function makeTable(row, col) {
+    const div = document.getElementById("showcase");
     const table = document.createElement("table");
     table.style = "border: 1px solid #444444;";
-    for(var i=0;i<col+1;i++) {
+    for(let i=0;i<col+1;i++) {
         const tr = document.createElement("tr");
-        for(var j=0;j<row;j++) {
+        for(let j=0;j<row;j++) {
             const td = document.createElement("td");
             if(i == 0) {
                 td.innerHTML = days[j % 7] + "요일";
@@ -37,22 +37,27 @@ function changeTable(row, col, name, colorindex) {
 }
 
 function message(s) {
-    document.getElementById("message").innerHTML = s;
+    const span = document.createElement("span");
+    const br = document.createElement("br");
+    span.innerHTML = s;
+    const div = document.getElementById("message");
+    div.appendChild(span);
+    div.appendChild(br);
 }
 
 makeTable(5, 12);
 
 document.getElementById("append").onclick = function() {
-    let s = document.getElementById("box").value;
-    let lec = document.getElementById("lecture").value;
-    let arr = s.split(',');
-    for(var i=0;i<arr.length;i++) {
-        let parsed = arr[i].trim();
-        let day = days.findIndex((elem) => elem == parsed[0]);
+    const s = document.getElementById("box").value;
+    const lec = document.getElementById("lecture").value;
+    const arr = s.split(',');
+    for(let i=0;i<arr.length;i++) {
+        const parsed = arr[i].trim();
+        const day = days.findIndex((elem) => elem == parsed[0]);
         if(day != -1) {
-            let twonum = parsed.split(" ")[1].trim().split("~");
+            const twonum = parsed.split(" ")[1].trim().split("~");
             if(twonum.length == 2) {
-                for(var j=parseInt(twonum[0]);j<=parseInt(twonum[1]);j++) {
+                for(let j=parseInt(twonum[0]);j<=parseInt(twonum[1]);j++) {
                     changeTable(day, j, lec, colori);
                 }
             } else if(twonum.length == 1) {
